08261623e2046bfd0edae84be3d32406efdd4f5d
Dario Piotrowicz <dario@cloudflare.com>
2024-03-01 00:09:27 +0000

    simplify reproduction

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,11 @@ export default async function Home() {
 
   const darthVader = ((await (await fetch('https://swapi.dev/api/people/4')).json()) as { name: string }).name;
 
-  const darthVaderA = ((await (await fetch('https://swapi.dev/api/people/4')).json()) as { name: string }).name;
-
   return (
     <main>
       <div>
         <p>
           {darthVader}
-        </p>
-        <p>
-          {darthVaderA}
         </p>
       </div>
     </main>
